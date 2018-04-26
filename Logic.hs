f9c89255aa49510871965c2004c679f04dd71814
Irfan Ali <cs16btech11019@iith.ac.in>
2018-04-26 13:25:16 +0530

    GetCoordinates using the tile number done

--- Logic.hs
+++ Logic.hs
@@ -6,6 +6,42 @@ import Data.Foldable ( asum )
 import Game
 import Graphics.Gloss.Interface.Pure.Game
 
+getCoords1:: Int -> Int -> (Int,Int)
+getCoords1 i j = if i == 7 then (580,300)
+								 else if i > 7 then (380 + (j-1)*40,260)
+								 else (380 + (j-1)*40,340)
+
+getCoords2:: Int -> Int -> (Int,Int)
+getCoords2 i j = if i == 7 then (300,20)
+								 else if i > 7 then (260,20 + (6-j)*40)
+								 else (340,20 + (6-j)*40)
+
+getCoords3:: Int -> Int -> (Int,Int)
+getCoords3 i j = if i == 7 then (20,300)
+								 else if i > 7 then (20 + (6-j)*40,340)
+								 else (20 + (6-j)*40,260)
+
+getCoords4:: Int -> Int -> (Int,Int)
+getCoords4 i j = if i == 7 then (300,580)
+								 else if i > 7 then (340,380 + (j-1)*40)
+								 else (260,380 + (j-1)*40)
+
+getCoords :: Int -> (Int,Int)
+getCoords 13 = (380,260)
+getCoords 26 = (260,220)
+getCoords 39 = (220,340)
+getCoords 52 = (340,380)
+
+getCoords a
+	| a <= 13 = if (mod a 13) < 7 then getCoords1 (mod a 13) (mod a 7)
+							else getCoords1 (mod a 13) (7- (mod a 7))
+	| a <= 26 = if (mod a 13) < 7 then getCoords2 (mod a 13) (1 + (mod a 7))
+							else getCoords2 (mod a 13) (6- (mod a 7))
+	| a <= 39 = if (mod a 13) < 7 then getCoords3 (mod a 13) (2+ (mod a 7))
+							else getCoords3 (mod a 13) (5- (mod a 7))
+	| otherwise = if (mod a 13) < 7 then getCoords4 (mod a 13) (3 + (mod a 7))
+							else getCoords4 (mod a 13) (4- (mod a 7))
+
 temp1 :: [(Int,Int)]
 temp1 = [(0,0)]
 temp2 :: [(Int,Int)]
