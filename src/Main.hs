db8319c63a658f06d63fb0a0c01977125706c82b
Irfan Ali <cs16btech11019@iith.ac.in>
2018-04-28 18:55:19 +0530

    Files arranged into folders

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import Data.Array
+import Graphics.Gloss
+import Graphics.Gloss.Data.Color
+
+import Rendering
+import Game
+import Logic
+
+
+window = InWindow "Functional" (screenWidth, screenHeight) (100, 100)
+backgroundColor = makeColor 255 255 255 128
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = play window backgroundColor 30 initialGame gameAsPicture transformGame (const id)
